ca2a1c8ce0d7ef0d6543b9667fed9854c1e3afc9
Daniel Marshall <daniel.marshall@contino.io>
2022-03-01 22:58:53 +1100

    Updated iam from containers deployment.

--- containers/main.tf
+++ containers/main.tf
@@ -20,17 +20,24 @@ module "ecr" {
 ### ECS
 ################################################################################
 
-module "terraform_ecs" {
-# Has deprecated resource references - possibly look at manual creation for now.
-  source  = "terraform-aws-modules/ecs/aws"
-  version = "3.4.1"
-  # insert the 1 required variable here
-  name               = var.set_ecs_name
-  create_ecs         = var.set_create_ecs
-  container_insights = var.set_container_insights
-  tags               = var.set_custom_tags
-}
+# module "terraform_ecs" {
+# # Has deprecated resource references - possibly look at manual creation for now.
+#   source  = "terraform-aws-modules/ecs/aws"
+#   version = "3.4.1"
+#   # insert the 1 required variable here
+#   name               = var.set_ecs_name
+#   create_ecs         = var.set_create_ecs
+#   container_insights = var.set_container_insights
+#   tags               = var.set_custom_tags
+# }
+
+# module "ecs" {
+#   source = "./modules/ecs"
+#   set_username_prefix = var.set_username_prefix
+
+# }
 
 ################################################################################
 ### Define outputs
 ################################################################################
+
